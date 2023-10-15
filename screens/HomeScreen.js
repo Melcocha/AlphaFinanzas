81d4d920c27c1b2dd933208a3382fc9783d989ef
MaicolPA <maikpa04@gmail.com>
2023-10-15 16:00:19 -0600

    Pantalla Home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,478 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+import MyChart from './MyChart';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import Fontisto from "react-native-vector-icons/Fontisto";
+import { Image } from "react-native";
 
 const HomeScreen = () => {
-
+  const navigation = useNavigation();
     return (
-        <View>
-            <Text style={{fontSize: 30,textAlign: "center",marginTop: "20%"}}>Homee Screen</Text>
-        </View>
-    );
-}
-
+        <>
+          <View style={styles.ingresos}>
+            <View style={styles.menuSup}>
+              <View style={styles.opciones}>
+                  <FontAwesome5 name="comment-dollar" size={24} color={"#27374D"} />
+                  <Text style={{ fontSize: 25, fontWeight: "700"}}> Total </Text>
+                  <AntDesign name="caretdown" size={18} color={"#27374D"} />
+              </View>
+              <View style={styles.opciones}>
+              <Text style={{fontSize: 25, fontWeight: "700"}}>$100</Text>
+              </View>
+              <View style={styles.BotonesP}>
+                <TouchableOpacity>
+                  <Text
+                    style={{
+                      fontSize: 24,
+                      fontWeight: "bold",
+                      color: "#FFC436",
+                      textDecorationLine: "underline",
+                    }}
+                  >
+                    INGRESOS
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate("HomeGastos")}>
+                  <Text style={styles.texto}>GASTOS</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.contenedorInf}>
+              <View style={styles.fechascont}>
+                <TouchableOpacity>
+                  <Text style={styles.fechasetxt}>Día</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Text style={styles.fechasetxtse}>Semana</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Text style={styles.fechasetxt}>Mes</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Text style={styles.fechasetxt}>Año</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ flex: 1, alignItems: "center"}}>
+                <MyChart />
+              </View>
+              <TouchableOpacity style={styles.btnAdd} onPress={() => navigation.navigate("HomeGastos")}>
+                <Text style={{ fontSize: 25 }}>+</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.contenedorInf2}>
+            <ScrollView style={styles.scrollcont}>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+            <View>
+              <Text style={{ paddingLeft: 10 }}>05 de octubre de 2023</Text>
+            </View>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.lista}>
+              <View>
+                <Image
+                  contentFit="cover"
+                  source={require("../assets/salario-1.png")}
+                />
+              </View>
+              <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 16, fontWeight: "700" }}>Salario</Text>
+                <Text>Salario Extra
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                    $100.00
+                  </Text>
+                  <Text>Principal</Text>
+                </View>
+              </View>
+            </View>
+          </ScrollView>
+            </View>
+          </View>
+        </>
+      );
+    };
+    const styles = StyleSheet.create({
+      ingresos: {
+        flex: 1,
+      },
+      menuSup: {
+        flex: 2,
+        alignItems: "center",
+        justifyContent: "center",
+        shadowOpacity: 1,
+      },
+      opciones: {
+        flexDirection: "row",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingRight: 35,
+        paddingLeft: 35,
+        paddingTop: 5,
+      },
+      btnCuenta: {
+        alignItems: "baseline",
+        flexDirection: "row",
+      },
+      btnBuscar: {
+        flexDirection: "row",
+      },
+      BotonesP: {
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25,
+        flexDirection: "row",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingRight: 45,
+        paddingLeft: 45,
+        marginBottom: 10,
+        paddingTop: 10, //arrecla el espacio para el telefono grande kevin
+      },
+      lista: {
+        flex: 1,
+        flexDirection: "row",
+        height: 80,
+        backgroundColor: "#fff",
+        borderRadius: 5,
+        marginLeft: 6,
+        marginRight: 6,
+        marginTop: 6,
+        alignItems: "center",
+        alignContent: "space-between",
+        padding: 10,
+        borderWidth: 2,
+        borderColor: "#a8a8a8",
+        shadowOpacity: 1,
+      },
+      texto: {
+        fontSize: 24,
+        fontWeight: "bold",
+      },
+      contenedorInf: {
+        flex: 4,
+        backgroundColor: "#fff",
+        borderRadius: 15,
+        margin: 8,
+        borderWidth: 2,
+        borderColor: "#a8a8a8",
+        shadowOpacity: 1,
+      },
+      contenedorInf2: {
+        flex: 2,
+        margin: 8,
+        shadowOpacity: 1,
+      },
+      fechascont: {
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-between",
+        paddingRight: 40,
+        paddingLeft: 40,
+        paddingTop: 10,
+      },
+      fechasetxt: {
+        fontSize: 22,
+        fontWeight: "500",
+      },
+      fechasetxtse: {
+        fontSize: 22,
+        fontWeight: "500",
+        textDecorationLine: "underline",
+        color: "#FFC436",
+      },
+      selectorfecha: {
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-between",
+        paddingRight: 20,
+        paddingLeft: 20,
+        paddingTop: 15,
+      },
+      selectorfechatxt: {
+        fontSize: 18,
+        fontWeight: "500",
+        textDecorationLine: "underline",
+      },
+      scrollcont: {
+        marginTop: 0,
+        marginBottom: 8,
+      },
+      btnAdd: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        backgroundColor: "#FFC436",
+        position: "absolute",
+        right: 0,
+        bottom: 0,
+        marginRight: 10,
+        marginBottom: 10,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    });
+    
 export default HomeScreen;
---
+++ src/navigation/DrawerNavigation.js
@@ -0,0 +1,118 @@
+
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../../screens/HomeScreen';
+import StackScreen from '../../screens/StackScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AjusteScreen from '../../screens/AjusteScreen';
+import AcercaScreen from '../../screens/Acerca';
+import AñadirTransaccionScreen from '../../screens/AñadirTransaccionScreen';
+import MoviGastosScreen from '../../screens/MoviGastosScreen';
+import MoviIngreScreen from '../../screens/MoviIngreScreen';
+import CategoriaGastosScreen from '../../screens/CategoriaGastosScreen';
+import CategoriaIngresosScreen from '../../screens/CategoriaIngresosScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from '../../screens/Login';
+import RegisterScreen from '../../screens/Register';
+import HomeScreenUser from '../../screens/HomeUser';
+import HomeScreenGastos from '../../screens/HomeScreenGastos';
+
+const HomeStackNavigator = createNativeStackNavigator();
+
+function StackMovimientos() {
+  return(
+    <HomeStackNavigator.Navigator>
+      <HomeStackNavigator.Screen
+      name="Ingresos"
+      component={MoviIngreScreen}
+      options={{headerShown:false, animation:"none"}}
+      />
+      <HomeStackNavigator.Screen
+      name="Gastos"
+      component={MoviGastosScreen}
+      options={{headerShown:false, animation:"none"}}
+      />
+      <HomeStackNavigator.Screen
+        name="AñadirTransaccion" // Cambia el nombre a "AñadirTransaccion"
+        component={AñadirTransaccionScreen}
+        options={{ headerShown: false, animation: "none" }}
+      />
+    </HomeStackNavigator.Navigator>
+  )
+}
+
+function StackcCategorias() {
+  return(
+    <HomeStackNavigator.Navigator>
+      <HomeStackNavigator.Screen
+      name="CategoriaIngresos"
+      component={CategoriaIngresosScreen}
+      options={{headerShown:false, animation:"none"}}
+      />
+      <HomeStackNavigator.Screen
+      name="CategoriaGastos"
+      component={CategoriaGastosScreen}
+      options={{headerShown:false, animation:"none"}}
+      />
+    </HomeStackNavigator.Navigator>
+  ) 
+}
+
+function StackHome() {
+  return(
+    <HomeStackNavigator.Navigator>
+      <HomeStackNavigator.Screen
+      name="HomeIngresos"
+      component={HomeScreen}
+      options={{headerShown:false, animation:"none"}}
+      />
+      <HomeStackNavigator.Screen
+      name="HomeGastos"
+      component={HomeScreenGastos}
+      options={{headerShown:false, animation:"none"}}
+      />
+    </HomeStackNavigator.Navigator>
+  ) 
+}
+
+const Drawer = createDrawerNavigator();
+
+export function DrawerNavigation() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "#27374D",
+          width: 300,
+        },
+        headerStyle: {
+          backgroundColor: "#27374D",
+          height: 100,
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "regular",
+          fontSize: 25,
+        },
+        drawerActiveTintColor: "#fff",
+        drawerLabelStyle: {
+          color: "#FFF",
+          fontSize: 18,
+        },
+      }}
+    >
+      <Drawer.Screen name="HomeUser" component={HomeScreenUser} options={{ drawerLabel: () => null }} />
+      <Drawer.Screen name="Inicio" component={StackHome} options={{drawerIcon: ({ color, size }) => (<Icon name="home" color="white" size={size} /> ),}}/>
+      <Drawer.Screen name="Movimientos" component={StackMovimientos} options={{drawerIcon: ({ color, size }) => (<Icon name="exchange" color="white" size={size} /> ),}}/> 
+      <Drawer.Screen name="Categorias" component={StackcCategorias} options={{drawerIcon: ({ color, size }) => (<Icon name="th-large" color="white" size={size} /> ),}}/>
+      <Drawer.Screen name="Ajustes" component={AjusteScreen} options={{drawerIcon: ({ color, size }) => (<Icon name="briefcase" color="white" size={size} /> ),}}/>
+      <Drawer.Screen name="Acerca De" component={AcercaScreen} options={{drawerIcon: ({ color, size }) => (<Icon name="comment-o" color="white" size={size} /> ),}}/>
+      <Drawer.Screen name="Cerrar Sesión" component={HomeScreen} options={{drawerIcon: ({ color, size }) => (<Icon name="sign-in" color="white" size={size} /> ),}}/>
+      <Drawer.Screen name="Stack" component={StackScreen} options={{drawerIcon: ({ color, size }) => (<Icon name="stack-exchange" color="white" size={size} /> ),}}/>
+      <Drawer.Screen name="Registro" component={RegisterScreen} options={{ drawerLabel: () => null }} />
+      <Drawer.Screen name="Login" component={LoginScreen} options={{ drawerLabel: () => null }} />
+    </Drawer.Navigator>
+  );
+}
+
